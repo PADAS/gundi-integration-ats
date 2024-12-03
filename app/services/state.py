250eddc2f70097d8dfca1d382636273658bc1a23
Mariano Martinez Grasso <marianomartinez84@gmail.com>
2024-12-03 13:58:51 -0300

    Support groups in the state manager

--- app/services/state.py
+++ app/services/state.py
@@ -35,6 +35,34 @@ class IntegrationStateManager:
                     f"integration_state.{integration_id}.{action_id}.{source_id}"
                 )
 
+    async def group_add(self, group_name: str, values: list):
+        # Adds values to a group. The group is created if it does not exist.
+        for attempt in stamina.retry_context(on=redis.RedisError, attempts=5, wait_initial=1.0, wait_max=30,
+                                             wait_jitter=3.0):
+            with attempt:
+                return await self.db_client.sadd(group_name, *values)
+
+    async def group_get(self, group_name: str):
+        # Gets all values in a group.
+        for attempt in stamina.retry_context(on=redis.RedisError, attempts=5, wait_initial=1.0, wait_max=30,
+                                             wait_jitter=3.0):
+            with attempt:
+                return await self.db_client.smembers(group_name)
+
+    async def group_move(self, from_group: str, to_group: str, values: list):
+        # Moves values from one group to another.
+        for attempt in stamina.retry_context(on=redis.RedisError, attempts=5, wait_initial=1.0, wait_max=30,
+                                             wait_jitter=3.0):
+            with attempt:
+                return await self.db_client.smove(from_group, to_group, *values)
+
+    async def group_remove(self, group_name: str, values: list):
+        # Removes values from a group.
+        for attempt in stamina.retry_context(on=redis.RedisError, attempts=5, wait_initial=1.0, wait_max=30,
+                                             wait_jitter=3.0):
+            with attempt:
+                return await self.db_client.srem(group_name, *values)
+
     def __str__(self):
         return f"IntegrationStateManager(host={self.db_client.host}, port={self.db_client.port}, db={self.db_client.db})"
 
