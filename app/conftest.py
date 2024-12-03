a1b3a446c0e4f7bc5b90feddaee66d2cf020350f
Mariano Martinez Grasso <marianomartinez84@gmail.com>
2024-12-03 14:33:26 -0300

    Test coverage for sets in the state manager

--- app/conftest.py
+++ app/conftest.py
@@ -70,6 +70,10 @@ def mock_redis(mocker, mock_integration_state):
     redis_client.decr.return_value = async_return(None)
     redis_client.expire.return_value = redis_client
     redis_client.execute.return_value = async_return((1, True))
+    redis_client.sadd.return_value = async_return(2)
+    redis_client.smembers.return_value = async_return(["file_3.xml", "file_4.xml"])
+    redis_client.smove.return_value = async_return(2)
+    redis_client.srem.return_value = async_return(2)
     redis_client.__aenter__.return_value = redis_client
     redis_client.__aexit__.return_value = None
     redis_client.pipeline.return_value = redis_client
--- app/services/tests/test_state_manager.py
+++ app/services/tests/test_state_manager.py
@@ -150,3 +150,57 @@ async def test_delete_state_source_state(mocker, mock_redis, integration_v2, moc
     mock_redis.Redis.return_value.delete.assert_called_once_with(
         f"integration_state.{integration_id}.pull_observations.{source_id}"
     )
+
+
+@pytest.mark.asyncio
+async def test_group_add(mocker, mock_redis, integration_v2):
+    mocker.patch("app.services.state.redis", mock_redis)
+    state_manager = IntegrationStateManager()
+    group_name = "pending_files"
+    files = ["file_1.xml", "file_2.xml"]
+
+    await state_manager.group_add(group_name=group_name, values=files)
+
+    mock_redis.Redis.return_value.sadd.assert_called_once_with(group_name, *files)
+
+
+@pytest.mark.asyncio
+async def test_group_get(mocker, mock_redis, integration_v2):
+    mocker.patch("app.services.state.redis", mock_redis)
+    state_manager = IntegrationStateManager()
+    group_name = "processed_files"
+    files = ["file_3.xml", "file_4.xml"]
+    await state_manager.group_add(group_name=group_name, values=files)
+
+    await state_manager.group_get(group_name=group_name)
+
+    mock_redis.Redis.return_value.smembers.assert_called_once_with(group_name)
+
+
+@pytest.mark.asyncio
+async def test_group_move(mocker, mock_redis, integration_v2):
+    mocker.patch("app.services.state.redis", mock_redis)
+    state_manager = IntegrationStateManager()
+    group_1 = "pending_files"
+    group_2 = "processed_files"
+    pending_files = ["file_1.xml", "file_2.xml"]
+    processed_files = ["file_3.xml", "file_4.xml"]
+    await state_manager.group_add(group_name=group_1, values=pending_files)
+    await state_manager.group_add(group_name=group_2, values=processed_files)
+
+    await state_manager.group_move(from_group=group_1, to_group=group_2, values=pending_files)
+
+    mock_redis.Redis.return_value.smove.assert_called_once_with(group_1, group_2, *pending_files)
+
+
+@pytest.mark.asyncio
+async def test_group_remove(mocker, mock_redis, integration_v2):
+    mocker.patch("app.services.state.redis", mock_redis)
+    state_manager = IntegrationStateManager()
+    group_name = "pending_files"
+    files = ["file_1.xml", "file_2.xml"]
+    await state_manager.group_add(group_name=group_name, values=files)
+
+    await state_manager.group_remove(group_name=group_name, values=files)
+
+    mock_redis.Redis.return_value.srem.assert_called_once_with(group_name, *files)
