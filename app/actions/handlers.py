bdc5552b8a51edf5c5db0bf23a985b5c5ebfbf02
Mariano Martinez Grasso <marianomartinez84@gmail.com>
2024-12-04 18:12:31 -0300

    Save ats responses as xml files in gcp

--- app/actions/handlers.py
+++ app/actions/handlers.py
@@ -1,17 +1,30 @@
 import datetime
+from enum import Enum
 import httpx
 import logging
 import stamina
+import aiofiles
 import app.actions.ats_client as client
 import app.services.gundi as gundi_tools
 from app.services.activity_logger import activity_logger
 from app.services.state import IntegrationStateManager
+from app.services.file_storage import CloudFileStorage
 from .configurations import PullObservationsConfig, ProcessObservationsConfig
 
 logger = logging.getLogger(__name__)
 
 
 state_manager = IntegrationStateManager()
+file_storage = CloudFileStorage()
+
+
+PENDING_FILES = "ats_pending_files"
+PROCESSED_FILES = "ats_processed_files"
+
+
+class FileStatus(Enum):
+    PENDING = "pending"
+    PROCESSED = "processed"
 
 
 def extract_gmt_offsets(transmissions, integration_id):
@@ -77,6 +90,7 @@ async def action_pull_observations(integration, action_config: PullObservationsC
         f"Executing pull_observations action with integration {integration} and action_config {action_config}..."
     )
     observations_extracted = 0
+    integration_id = str(integration.id)
     try:
         # ToDo: Review retry logic
         async for attempt in stamina.retry_context(
@@ -86,23 +100,55 @@ async def action_pull_observations(integration, action_config: PullObservationsC
                 wait_max=datetime.timedelta(seconds=10),
         ):
             with attempt:
-                data_points_per_device = await client.get_data_endpoint_response(
-                    integration_id=str(integration.id),
+                # ToDo: Add extra logging
+                auth_config = client.get_auth_config(integration)
+                transmissions_raw_xml = await client.get_transmissions_endpoint_response(
+                    integration_id=integration_id,
                     config=client.get_pull_config(integration),
-                    auth=client.get_auth_config(integration)
+                    auth=auth_config
+                )
+                # Save xml to a file and upload it to cloud storage
+                timestamp = datetime.datetime.now(tz=datetime.timezone.utc).strftime("%Y%m%d%H%M%S%f")
+                transmissions_file_name = f"{timestamp}_{integration_id}_transmissions.xml"
+                async with aiofiles.open(f"/tmp/{transmissions_file_name}", "w") as f:
+                    await f.write(transmissions_raw_xml)
+                await file_storage.upload_file(
+                    integration_id=integration_id,
+                    local_file_path=f"/tmp/{transmissions_file_name}",
+                    destination_blob_name=transmissions_file_name,
+                    metadata={"integration_id": integration_id, "ats_username": auth_config.username}
+                )
+                data_points_file_name = f"{timestamp}_{integration_id}_data_points.xml"
+                # Save xml to a file and upload it to cloud storage
+                data_points_raw_xml = await client.get_data_endpoint_response(
+                    integration_id=integration_id,
+                    config=client.get_pull_config(integration),
+                    auth=auth_config
+                )
+                async with aiofiles.open(f"/tmp/{data_points_file_name}", "w") as f:
+                    await f.write(data_points_raw_xml)
+                await file_storage.upload_file(
+                    integration_id=integration_id,
+                    local_file_path=f"/tmp/{data_points_file_name}",
+                    destination_blob_name=data_points_file_name,
+                    metadata={
+                        "integration_id": integration_id,
+                        "ats_username": auth_config.username,
+                        "status": "pending"
+                    }
                 )
 
-                if data_points_per_device:
-                    transmissions = await client.get_transmissions_endpoint_response(
-                        integration_id=str(integration.id),
-                        config=client.get_pull_config(integration),
-                        auth=client.get_auth_config(integration)
-                    )
-                    observations_extracted += len(data_points_per_device)
-                else:
-                    logger.warning(f"No observations were pulled for integration ID: {str(integration.id)}.")
-                    # ToDo: Log a warning in the activity logs too
-                    return {"message": "No observations pulled"}
+                # Add it to the list of pending files to be processed
+                await state_manager.group_add(
+                    group_name=PENDING_FILES,
+                    values=[data_points_file_name]
+                )
+
+                # ToDo: check if there's data inside the xml? or move to the processor action?
+                # if not data_points_per_device:
+                #     logger.warning(f"No observations were pulled for integration ID: {str(integration.id)}.")
+                #     # ToDo: Log a warning in the activity logs too
+                #     return {"message": "No observations pulled"}
     # ToDo: Review error handling
     except httpx.HTTPError as e:
         message = f"Error fetching data points/transmissions from ATS. Integration ID: {str(integration.id)} Exception: {e}"
