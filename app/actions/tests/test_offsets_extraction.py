004229802b9d710b84901b736379afc3980deda5
Mariano Martinez Grasso <marianomartinez84@gmail.com>
2024-12-09 10:31:41 -0300

    Refactor tests for tz offset extractions

--- app/actions/tests/test_offsets_extraction.py
+++ app/actions/tests/test_offsets_extraction.py
@@ -2,71 +2,34 @@ from app.actions.ats_client import PullObservationsTransmissionsResponse
 from app.actions.handlers import extract_gmt_offsets
 
 
-TRANSMISSIONS_RESPONSE = [
-    {
-        '@diffgr:id': 'Table1',
-        '@msdata:rowOrder': '0',
-        'DateSent': '2024-10-31T20:24:34.67+00:00',
-        'CollarSerialNum': '053506',
-        'NumberFixes': '0',
-        'BattVoltage': '7.056',
-        'Mortality': 'No',
-        'BreakOff': 'No',
-        'SatErrors': '5',
-        'YearBase': '20',
-        'DayBase': '0',
-        'GmtOffset': '0',
-        'Event': 'None,None,None',
-        'evCondition': 'None,None,None',
-        'LowBattVoltage': 'false'
-    },
-    {
-        '@diffgr:id': 'Table2',
-        '@msdata:rowOrder': '1',
-        'DateSent': '2024-10-30T20:36:19.653+06:00',
-        'CollarSerialNum': '053507',
-        'NumberFixes': '0',
-        'BattVoltage': '6.984',
-        'Mortality': 'No',
-        'BreakOff': 'No',
-        'SatErrors': '6',
-        'YearBase': '20',
-        'DayBase': '0',
-        'GmtOffset': '6',
-        'Event': 'None,None,None',
-        'evCondition': 'None,None,None',
-        'LowBattVoltage': 'false'
-    },
-    {
-        '@diffgr:id': 'Table3',
-        '@msdata:rowOrder': '2',
-        'DateSent': '2024-10-27T20:16:09.653+07:00',
-        'CollarSerialNum': '053508',
-        'NumberFixes': '0',
-        'BattVoltage': '6.984',
-        'Mortality': 'No',
-        'BreakOff': 'No',
-        'SatErrors': '6',
-        'YearBase': '20',
-        'DayBase': '0',
-        'GmtOffset': '7',
-        'Event': 'None,None,None',
-        'evCondition': 'None,None,None',
-        'LowBattVoltage': 'false'
+def test_extract_gmt_offsets_from_transmissions(ats_integration_v2, mock_ats_transmissions_parsed):
+    integration_id = str(ats_integration_v2.id)
+    transmissions = mock_ats_transmissions_parsed
+    expected_gmt_offsets = {i.collar_serial_num: i.gmt_offset for i in transmissions}
+
+    extracted_gmt_offsets = extract_gmt_offsets(transmissions, integration_id)
+
+    assert extracted_gmt_offsets == expected_gmt_offsets
+
+
+def test_extract_gmt_offsets_from_transmissions_with_invalid_offsets(
+        ats_integration_v2, mock_ats_transmissions_with_invalid_offsets_parsed
+):
+    integration_id = str(ats_integration_v2.id)
+    transmissions = mock_ats_transmissions_with_invalid_offsets_parsed
+    expected_gmt_offsets = {
+        i.collar_serial_num: i.gmt_offset
+        for i in transmissions
+        if i.gmt_offset is not None and -12 <= i.gmt_offset <= 14
     }
-]
+
+    extracted_gmt_offsets = extract_gmt_offsets(transmissions, integration_id)
+
+    assert extracted_gmt_offsets == expected_gmt_offsets
 
 
-def test_extract_gmt_offsets_from_transmissions():
-    integration_id = "779ff3ab-5589-4f4c-9e0a-ae8d6c9edff0"
-    transmissions = PullObservationsTransmissionsResponse.parse_obj({"transmissions": TRANSMISSIONS_RESPONSE})
-    test_gmt_offsets = {i.collar_serial_num: i.gmt_offset for i in transmissions.transmissions}
-    extracted_gmt_offsets = extract_gmt_offsets(transmissions.transmissions, integration_id)
-
-    assert test_gmt_offsets == extracted_gmt_offsets
-
-def test_extract_gmt_offsets_with_no_transmissions(caplog):
-    integration_id = "779ff3ab-5589-4f4c-9e0a-ae8d6c9edff0"
+def test_extract_gmt_offsets_with_no_transmissions(ats_integration_v2, caplog):
+    integration_id = str(ats_integration_v2.id)
     transmissions = {}
     extracted_gmt_offsets = extract_gmt_offsets(transmissions, integration_id)
 
