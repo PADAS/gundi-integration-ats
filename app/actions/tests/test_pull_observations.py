8fa8f6311414d31e2e6acd8ba2ff9f731968097b
Victor H. García <victorhugogarcia13d@gmail.com>
2025-09-08 16:27:38 -0600

    Remove unused imports

--- app/actions/tests/test_pull_observations.py
+++ app/actions/tests/test_pull_observations.py
@@ -1,6 +1,6 @@
 import pytest
 from app.services.action_runner import execute_action
-from app.actions.handlers import PENDING_FILES, retrieve_transmissions, retrieve_data_points
+from app.actions.handlers import PENDING_FILES
 
 
 @pytest.mark.asyncio
